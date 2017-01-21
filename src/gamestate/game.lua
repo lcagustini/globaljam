890aba031d0fcd06c30fa4727abccb044d2f43fa
Logan <joaospuri1@hotmail.com>
2017-01-21 14:29:05 -0200

    merged with master

--- src/gamestate/game.lua
+++ src/gamestate/game.lua
@@ -1,27 +1,52 @@
 local gamestate = require "src.lib.gamestate"
+require "src.wave"
 
 local game = {}
 
-local physicsWorld = {}
-
 local gameCanvas = love.graphics.newCanvas()
+local gameTime;
 
-function game:enter() 
-    circle = {
-               x = 100,
-               y = 100,
-               r = 20
-             }
+function game:enter()
+    map = require "src.objects.map"
+    map:init()
+
+    love.graphics.setLineWidth(5)
+  
+      gameTime = 0;
+    
+    --Testing waves
+    waves = { }
+    testWaveTable = {{releaseTime=0.5, track=1, speed=100}, {releaseTime=1.5, track=1, speed=200}, {releaseTime=2.0,track=1, speed=300}}
 end
 
+
 function game:update(dt)
+    --Waves
+    gameTime = gameTime+dt
+    updateWaves(waves, testWaveTable, gameTime, dt)
+    
+	--Teste
     love.graphics.setCanvas(gameCanvas)
-        love.graphics.circle("fill", circle.x, circle.y, circle.r)
+  
+    love.graphics.clear()
+    drawWaves(waves)
+
+    for i,j in ipairs(map.towers) do
+        love.graphics.circle("fill", j.x, j.y, 10)
+    end
+    for i,j in ipairs(map.paths) do
+        love.graphics.setColor(map.paths.colors[i])
+        love.graphics.line(j)
+    end
+    love.graphics.setColor(255,255,255)
+
     love.graphics.setCanvas()
+   
+    
 end
 
 function game:draw()
-    love.graphics.print(love.timer.getFPS(), 0, 0)
+    love.graphics.print(gameTime, 0, 0)
     love.graphics.draw(gameCanvas)
 end
 
