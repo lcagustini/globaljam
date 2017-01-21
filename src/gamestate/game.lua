772ea6e3beba63ee518858a8e967f2a2cccf5cef
lcagustini <torresmon235@gmail.com>
2017-01-21 16:35:38 +0000

    Refactor

--- src/gamestate/game.lua
+++ src/gamestate/game.lua
@@ -7,13 +7,15 @@ local gameCanvas = love.graphics.newCanvas()
 local gameTime;
 
 function game:enter()
+    --Initialize map with towers and paths
     map = require "src.objects.map"
     map:init()
 
+    --Path width
     love.graphics.setLineWidth(5)
-  
-      gameTime = 0;
-    
+
+    gameTime = 0;
+
     --Testing waves
     waves = { }
     testWaveTable = {{releaseTime=0.5, track=1, speed=100}, {releaseTime=1.5, track=1, speed=200}, {releaseTime=2.0,track=1, speed=300}}
@@ -24,29 +26,19 @@ function game:update(dt)
     --Waves
     gameTime = gameTime+dt
     updateWaves(waves, testWaveTable, gameTime, dt)
-    
-	--Teste
-    love.graphics.setCanvas(gameCanvas)
-  
-    love.graphics.clear()
-    drawWaves(waves)
 
-    for i,j in ipairs(map.towers) do
-        love.graphics.circle("fill", j.x, j.y, 10)
-    end
-    for i,j in ipairs(map.paths) do
-        love.graphics.setColor(map.paths.colors[i])
-        love.graphics.line(j)
-    end
-    love.graphics.setColor(255,255,255)
+    --Drawing
+    love.graphics.setCanvas(gameCanvas)
+    love.graphics.clear()
+
+    drawWaves(waves)
+    map:render()
 
     love.graphics.setCanvas()
-   
-    
 end
 
 function game:draw()
-    love.graphics.print(gameTime, 0, 0)
+    love.graphics.print(love.timer.getFPS(), 0, 0)
     love.graphics.draw(gameCanvas)
 end
 
--- src/objects/map.lua
+++ src/objects/map.lua
@@ -1,30 +1,46 @@
 local map = {}
 
-function map:init()
-    map.towers = getTowers()
-    map.paths = getPaths()
-end
-
-function getTowers()
+local function getTowers()
     local towers = {}
+    --Creates towers in random places
     for i=1,15,1 do
         towers[i] = {x = love.math.random(800), y = love.math.random(600)}
     end
     return towers
 end
 
-function getPaths()
+local function getPaths()
     local cr = require "src.lib.cornerRounder"
 
     local paths = {}
     paths.colors = {}
 
+    --Corner rounded paths with unique colors
     paths[1] = cr.tranform_line_points({200,600, 200,500, 400,500, 400,200, 800,200}, 20)
     paths.colors[1] = {0,255,0}
+
     paths[2] = cr.tranform_line_points({450,600, 450,400, 100,400, 100,250, 150,250, 150,0}, 20)
     paths.colors[2] = {255,0,0}
 
     return paths
+end
+
+function map:init()
+    map.towers = getTowers()
+    map.paths = getPaths()
+end
+
+function map:render()
+    --Renders the towers
+    for i,j in ipairs(map.towers) do
+        love.graphics.circle("fill", j.x, j.y, 10)
+    end
+    --Renders the paths
+    for i,j in ipairs(map.paths) do
+        love.graphics.setColor(map.paths.colors[i])
+        love.graphics.line(j)
+    end
+    love.graphics.setColor(255,255,255)
 end
 
 return map
