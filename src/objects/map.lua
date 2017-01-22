ce1416330d8355f3d03ef9f53082a86725aaa185
AkariUeda <ueda.aka@gmail.com>
2017-01-22 01:07:13 -0200

    att torre.lua

--- src/objects/map.lua
+++ src/objects/map.lua
@@ -1,20 +1,6 @@
 local map = {}
+local towers = require "src.objects.torre"
 
-local function getTowers()
-    local towers = {}
-    --Creates towers
-    
-      towers[1] = {x = 170, y = 120}
-      towers[2] = {x = 250, y = 380}
-      towers[3] = {x = 360, y = 440}
-      towers[4] = {x = 370, y = 212}
-      towers[5] = {x = 550, y = 337}
-      towers[6] = {x = 650, y = 450}
-      towers[7] = {x = 650, y = 75}
-      towers[8] = {x = 750, y = 225}
-    
-    return towers
-end
 
 local function getPaths()
     local cr = require "src.lib.cornerRounder"
@@ -37,8 +23,10 @@ local function getPaths()
     return paths
 end
 
+
 function map:init()
-    self.towers = getTowers()
+
+    self.towers = towers:getTowers()
     self.paths = getPaths()
 end
 
