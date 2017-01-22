651a79f4daa44225b55385d29d71efb1435d9cef
AkariUeda <ueda.aka@gmail.com>
2017-01-22 16:17:00 -0200

    creditos arrumados

--- src/gamestate/info.lua
+++ src/gamestate/info.lua
@@ -3,11 +3,17 @@ local gamestate = require "src.lib.gamestate"
 local info = {}
 
 function info:enter()
-    infoimage = love.graphics.newImage("assets/creditos-deuonda.png")
+    infoimage = love.graphics.newImage("assets/creditos-deuonda1.png")
 end
 
 function info:draw()
     love.graphics.draw(infoimage, 0 , 0)
+end
+
+function info:mousepressed(x,y,button)
+  if button == 1 and x >= 350 and x <= 430 and y >= 470 and y <= 550   then
+    gamestate.switch(menu)
+  end
 end
 
 return info
